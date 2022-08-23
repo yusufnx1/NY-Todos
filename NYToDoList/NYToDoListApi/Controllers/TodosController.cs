6b7a79dafd28bf5bbc1f62712ffb96714b1e472b
Yusuf Önder <onderyusuf067@gmail.com>
2022-08-23 10:55:44 +0300

    NY-Todos
    NY-Todos

---
+++ NYToDoList/NYToDoListApi/Controllers/TodosController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NYToDoListApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NYToDoListApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        private readonly ToDoRepository doRepository;
+
+        public TodosController()
+        {
+            doRepository = new ToDoRepository();
+        }
+        [HttpGet]
+        public IEnumerable<ToDo> Get()
+        {
+            return doRepository.GetAll();
+        }
+        [HttpGet("{id}")]
+        public ToDo Get(int id)
+        {
+            return doRepository.GetById(id);
+        }
+        [HttpPost]
+        public void Post([FromBody] ToDo to)
+        {
+            if (ModelState.IsValid)
+            {
+                doRepository.AddTo(to);
+            }
+        }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] ToDo to)
+        {
+            to.Id = id;
+            if (ModelState.IsValid)
+            {
+                doRepository.Update(to);
+            }
+        }
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            doRepository.Delete(id);
+        }
+    }
+}
---
+++ NYToDoList/NYToDoListApi/Model/ToDoRepository.cs
@@ -0,0 +1,72 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NYToDoListApi.Model
+{
+    public class ToDoRepository
+    {
+        private string connectionString = "";
+
+        public ToDoRepository()
+        {
+            connectionString = "Data Source=DESKTOP-N6665N4\\HP;Initial Catalog=Todo;Integrated Security=true";
+        }
+        public IDbConnection connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+            }     
+        }
+        public void AddTo(ToDo to)
+        {
+            using (IDbConnection dbconnection=connection)
+            {
+                string sQuery = "INSERT INTO Todos (Title,Text,Date,Status) VALUES (@Title,@Text,@Date,@Status)";
+                dbconnection.Open();
+                dbconnection.Execute(sQuery,to);
+            }
+        }
+        public IEnumerable<ToDo> GetAll()
+        {
+            using (IDbConnection dbconnection = connection)
+            {
+                string sQuery = "SELECT * FROM Todos";
+                dbconnection.Open();
+                return dbconnection.Query<ToDo>(sQuery);
+            }
+        }
+        public ToDo GetById(int Id)
+        {
+            using (IDbConnection dbconnection = connection)
+            {
+                string sQuery = "SELECT * FROM Todos where Id=@Id";
+                dbconnection.Open();
+                return dbconnection.Query<ToDo>(sQuery, new { Id = @Id }).FirstOrDefault();
+            }
+        }
+        public void Delete(int Id)
+        {
+            using (IDbConnection dbconnection = connection)
+            {
+                string sQuery = "DELETE FROM Todos where Id=@Id";
+                dbconnection.Open();
+                dbconnection.Query<ToDo>(sQuery, new { Id = @Id });
+            }
+        }
+        public void Update(ToDo to)
+        {
+            using (IDbConnection dbconnection = connection)
+            {
+                string sQuery = "UPDATE Todos SET Title=@Title, Text=@Text, Date=@Date, Status=@Status where Id=@Id";
+                dbconnection.Open();
+                dbconnection.Query<ToDo>(sQuery,to);
+            }
+        }
+    }
+}
